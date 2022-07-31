46e8d3260d6c32df28e216fc26c26a3cbc302d27
Natã Vitoriano <natavitoriano@hotmail.com>
2022-07-31 12:29:19 -0300

    aula 49

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -19,6 +19,9 @@ class FornecedorController extends Controller
             ]
         ];
 
+        $msg = isset($fornecedores[0]['cnpj']) ? 'CNPJ informado' : 'CNPJ não informado';
+        echo $msg;
+
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
