429a1001ecc87363e8b7f498ef45bb5c4910740e
Natã Vitoriano <natavitoriano@hotmail.com>
2022-07-31 12:55:53 -0300

    aula 51

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -45,6 +45,20 @@ Status: {{ $fornecedores[0]['status']}}
     <br />
     CNPJ: {{ $fornecedores[0]['cnpj'] ?? 'Dado não foi preenchido'}}
     <br />
+    Telefone: ({{$fornecedores[0]['ddd'] ?? ''}}) {{$fornecedores[0]['telefone'] ?? ''}}
+    @switch($fornecedores[0]['ddd'])
+        @case('11')
+            São Paulo - SP
+            @break
+        @case('32')
+            Juiz de Fora - MG
+            @break
+        @case('85')
+            Fortaleza - CE
+            @break
+        @default
+            Estado não identificado.
+    @endswitch
     {{--
     @isset($fornecedores[0]['cnpj'])
         CNPJ: {{ $fornecedores[0]['cnpj']}}
