be2c67b5e35f9d4c4cc4e1b1ffede8d6559f2f38
Natã Vitoriano <natavitoriano@hotmail.com>
2022-07-31 06:41:45 -0300

    aula 37

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -12,16 +12,16 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', 'PrincipalController@principal');
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-Route::get('/contato', 'ContatoController@contato');
-Route::get('/login', function() { return 'Login'; });
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function() { return 'Login'; })->name('site.login');
 
 //App
 Route::prefix('/app')->group(function() {
-    Route::get('/clientes', function() { return 'Clientes'; });
-    Route::get('/fornecedores', function() { return 'Fornecedores'; });
-    Route::get('/produtos', function() { return 'Produtos'; });
+    Route::get('/clientes', function() { return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', function() { return 'Fornecedores'; })->name('app.fornecedores');
+    Route::get('/produtos', function() { return 'Produtos'; })->name('app.produtos');
 });
 
 /*
