3630e0016680e6c5ec396a8cc23b6076eb1fc821
Natã Vitoriano <natavitoriano@hotmail.com>
2022-07-30 21:07:17 -0300

    first commit

---
+++ app_super_gestao/routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', 'PrincipalController@principal');
+
+//Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+
+/*
+Route::get('/', function () {
+    return 'Olá, seja bem-vindo ao curso!';
+});*/
+
+Route::get('/sobre-nos', function () {
+    return 'Sobre-nós';
+});
+
+Route::get('/contato', function () {
+    return 'Contato';
+});
+
+/* verbo http
+get
+post
+put
+patch
+delete
+options
+*/
