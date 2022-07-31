47b77654eaa2ec511137a838e3e8ddc697c3a3d6
Natã Vitoriano <natavitoriano@hotmail.com>
2022-07-31 06:14:19 -0300

    finalizando aula 31

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -18,6 +18,12 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
 
+Route::get(
+    '/contato/{nome}/{categoria}/{assunto}/{mensagem}',
+    function(string $nome, string $categoria, string $assunto, string $mensagem) {
+        echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+});
+
 //Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
 
 /*
