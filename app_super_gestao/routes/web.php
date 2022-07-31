7d982f99d904ff8a4be33ffc5eaa46684227b096
Natã Vitoriano <natavitoriano@hotmail.com>
2022-07-31 06:36:48 -0300

    aula 36

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -13,18 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', 'PrincipalController@principal');
-
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-
 Route::get('/contato', 'ContatoController@contato');
-
 Route::get('/login', function() { return 'Login'; });
 
-Route::get('/clientes', function() { return 'Clientes'; });
+//App
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function() { return 'Clientes'; });
+    Route::get('/fornecedores', function() { return 'Fornecedores'; });
+    Route::get('/produtos', function() { return 'Produtos'; });
+});
 
-Route::get('/fornecedores', function() { return 'Fornecedores'; });
-
-Route::get('/produtos', function() { return 'Produtos'; });
 /*
 Route::get(
     '/contato/{nome}/{categoria_id}',
