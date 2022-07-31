3a15007755f2623dd7b57edf1d423524451a03cc
Natã Vitoriano <natavitoriano@hotmail.com>
2022-07-31 10:14:24 -0300

    aula 43

---
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,10 @@
+<h3>Fornecedores</h3>
+
+{{--Fica o comentário que será descartado pelo interpretador do blade--}}
+
+@php
+    //Para comentários de uma linha
+    /*
+    Multiplas linhas
+    */
+@endphp
