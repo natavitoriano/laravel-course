46d59f36c33770aace79801c8bd052f967983605
Natã Vitoriano <natavitoriano@hotmail.com>
2022-07-31 10:21:12 -0300

    aula 45

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -7,4 +7,20 @@
     /*
     Multiplas linhas
     */
+    /*
+    if(){
+
+    } elseif() {
+
+    } else {
+
+    }*/
 @endphp
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existe alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Não existem fornecedores cadastrados</h3>
+@endif
