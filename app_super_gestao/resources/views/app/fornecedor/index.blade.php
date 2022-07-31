75fc9810c57e572cd06fb5d9854260a6574fa520
Natã Vitoriano <natavitoriano@hotmail.com>
2022-07-31 10:32:22 -0300

    aula 46

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -16,7 +16,7 @@
 
     }*/
 @endphp
-
+{{--
 @if(count($fornecedores) > 0 && count($fornecedores) < 10)
     <h3>Existe alguns fornecedores cadastrados</h3>
 @elseif(count($fornecedores) > 10)
@@ -24,3 +24,16 @@
 @else
     <h3>Não existem fornecedores cadastrados</h3>
 @endif
+--}}
+
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br />
+Status: {{ $fornecedores[0]['status']}}
+<br />
+@if($fornecedores[0]['status'] == 'N')
+    Fornecedor inativo
+@endif
+<br>
+@unless($fornecedores[0]['status'] == 'S')<!-- se o retorno da condição for falso-->
+    Fornecedor inativo
+@endunless
